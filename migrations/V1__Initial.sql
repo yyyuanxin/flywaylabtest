2f036f02cc6df97aaf143e195048ca4a886b11b2
yuanxin11044 <yuanxin11044@gmail.com>
2022-02-22 14:50:24 +0800

    Update initial sql

--- migrations/V1__Initial.sql
+++ migrations/V1__Initial.sql
@@ -4,3 +4,8 @@ CREATE TABLE HelloWorld (
     CONSTRAINT PK_HelloWorld PRIMARY KEY CLUSTERED (HelloWorldId)
 );
 
+CREATE TABLE HelloWorld2 (
+    HelloWorldId2 INT IDENTITY NOT NULL,
+    Declaration2 nvarchar(1000) NULL,
+    CONSTRAINT PK_HelloWorld PRIMARY KEY CLUSTERED (HelloWorldId2)
+);
